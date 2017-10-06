9ed3a3d062eaaf358c4bcde0318e643bb0a1fefa
Tedd Mason <teddmason@gmail.com>
2017-10-06 10:20:43 +0100

    tests

---
+++ test/getMessage.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const Code = require('code')
+const getMessage = require('../lib/functions/getMessage').getMessage
+const database = require('../lib/helpers/database')
+let event
+
+lab.experiment('getMessage', () => {
+  lab.beforeEach((done) => {
+    event = {
+      pathParameters: {
+        id: '4eb3b7350ab7aa443650fc9351f'
+      }
+    }
+    // mock database query
+    database.queryVars = (query, params, callback) => {
+      callback(null, {
+        rows: [{
+          alert: '<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>'
+        }]
+      })
+    }
+    done()
+  })
+
+  lab.test('Correct data test', (done) => {
+    getMessage(event, {}, (err, ret) => {
+      Code.expect(err).to.be.null()
+      Code.expect(ret.statusCode).to.equal(200)
+      Code.expect(ret.headers['content-type']).to.equal('application/xml')
+      Code.expect(ret.body).to.equal('<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>')
+      done()
+    })
+  })
+
+  lab.test('No data found test', (done) => {
+    database.queryVars = (query, params, callback) => {
+      callback(null, {
+        rows: []
+      })
+    }
+    getMessage(event, {}, (err, ret) => {
+      Code.expect(err).to.be.an.error()
+      Code.expect(err).to.equal(new Error('No message found'))
+      Code.expect(ret).to.be.undefined()
+      done()
+    })
+  })
+
+  lab.test('Error test', (done) => {
+    database.queryVars = (query, params, callback) => {
+      callback(new Error('test error'))
+    }
+    getMessage(event, {}, (err, ret) => {
+      Code.expect(err).to.be.an.error()
+      Code.expect(ret).to.be.undefined()
+      done()
+    })
+  })
+})
--- test/getMessagesAtom.js
+++ test/getMessagesAtom.js
@@ -3,18 +3,18 @@
 const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const Code = require('code')
-const getMessages = require('../lib/functions/getMessages').getMessages
+const getMessagesAtom = require('../lib/functions/getMessagesAtom').getMessagesAtom
 const database = require('../lib/helpers/database')
-let capAlert
 
-lab.experiment('getMessages', () => {
+lab.experiment('getMessagesAtom', () => {
   lab.beforeEach((done) => {
-    capAlert = require('./data/capAlert.json')
     // mock database query
     database.query = (params, callback) => {
       callback(null, {
         rows: [{
-          alert: '<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>'
+          alert: '<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>',
+          sent: new Date(),
+          identifier: '4eb3b7350ab7aa443650fc9351f'
         }]
       })
     }
@@ -22,11 +22,10 @@ lab.experiment('getMessages', () => {
   })
 
   lab.test('Correct data test', (done) => {
-    getMessages(capAlert, {}, (err, ret) => {
+    getMessagesAtom({}, {}, (err, ret) => {
       Code.expect(err).to.be.null()
       Code.expect(ret.statusCode).to.equal(200)
       Code.expect(ret.headers['content-type']).to.equal('application/xml')
-      Code.expect(ret.body).to.equal('<?xml version="1.0" standalone="yes"?>\n<messages>\n<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>\n</messages>')
       done()
     })
   })
@@ -35,7 +34,7 @@ lab.experiment('getMessages', () => {
     database.query = (params, callback) => {
       callback(new Error('test error'))
     }
-    getMessages(capAlert, {}, (err, ret) => {
+    getMessagesAtom({}, {}, (err, ret) => {
       Code.expect(err).to.be.an.error()
       Code.expect(ret).to.be.undefined()
       done()
