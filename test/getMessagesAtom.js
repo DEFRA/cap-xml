f84dc46de74a1435420bf12c52336e1379d4b061
Tedd Mason <teddmason@gmail.com>
2018-05-24 13:56:39 +0100

    Node 8 work, tests and getmessage
    getMessagesAtom, unit tests and a fix for getmessage
    
    Changing to use a service helper
    
    Processmessage
    
    Api gateway error handling needs promises returned from lambda
    
    Reverting to async functions from promise returns
    
    Updating database types in database
    
    package version

--- test/getMessagesAtom.js
+++ test/getMessagesAtom.js
@@ -4,40 +4,65 @@ const Lab = require('lab')
 const lab = exports.lab = Lab.script()
 const Code = require('code')
 const getMessagesAtom = require('../lib/functions/getMessagesAtom').getMessagesAtom
-const database = require('../lib/helpers/database')
+const service = require('../lib/helpers/service')
 
 lab.experiment('getMessagesAtom', () => {
-  lab.beforeEach((done) => {
+  lab.beforeEach(() => {
     // mock database query
-    database.query = (params, callback) => {
-      callback(null, {
-        rows: [{
-          alert: '<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>',
-          sent: new Date(),
-          identifier: '4eb3b7350ab7aa443650fc9351f'
-        }]
+    service.getAllMessages = (query) => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          rows: [{
+            alert: '<alert xmlns="urn:oasis:names:tc:emergency:cap:1.2">test</alert>',
+            sent: new Date(),
+            identifier: '4eb3b7350ab7aa443650fc9351f'
+          }]
+        })
       })
     }
-    done()
   })
 
-  lab.test('Correct data test', (done) => {
-    getMessagesAtom({}, {}, (err, ret) => {
-      Code.expect(err).to.be.null()
-      Code.expect(ret.statusCode).to.equal(200)
-      Code.expect(ret.headers['content-type']).to.equal('application/xml')
-      done()
-    })
+  lab.test('Correct data test', async () => {
+    const ret = await getMessagesAtom({})
+    Code.expect(ret.statusCode).to.equal(200)
+    Code.expect(ret.headers['content-type']).to.equal('application/xml')
   })
 
-  lab.test('Error test', (done) => {
-    database.query = (params, callback) => {
-      callback(new Error('test error'))
+  lab.test('Bad rows returned', async () => {
+    service.getAllMessages = (query) => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          rows: 1
+        })
+      })
     }
-    getMessagesAtom({}, {}, (err, ret) => {
+    const ret = await getMessagesAtom({})
+    Code.expect(ret.statusCode).to.equal(200)
+    Code.expect(ret.headers['content-type']).to.equal('application/xml')
+  })
+
+  lab.test('No return from database', async () => {
+    service.getAllMessages = (query) => {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    }
+    const ret = await getMessagesAtom({})
+    Code.expect(ret.statusCode).to.equal(200)
+    Code.expect(ret.headers['content-type']).to.equal('application/xml')
+  })
+
+  lab.test('Error test', async () => {
+    service.getAllMessages = (query) => {
+      return new Promise((resolve, reject) => {
+        reject(new Error('test error'))
+      })
+    }
+    try {
+      await getMessagesAtom({})
+    } catch (err) {
       Code.expect(err).to.be.an.error()
-      Code.expect(ret).to.be.undefined()
-      done()
-    })
+      Code.expect(err.message).to.equal('test error')
+    }
   })
 })
