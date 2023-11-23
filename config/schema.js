f9315f7fff0223f1286ec776346ecb56831c22ba
Colin Barker (A513205) <41692478+ColinB-A513205@users.noreply.github.com>
2023-11-23 11:13:25 +0000

    Service terraform migration config (#35)
    * Updates for new dev service built with terraform
    
    * Updates for new dev service built with terraform
    
    * naming convention updates
    
    * updated dv to dev
    
    * environment naming updates

--- config/schema.js
+++ config/schema.js
@@ -9,7 +9,7 @@ module.exports = Joi.object({
     accountId: Joi.string().required().allow('', null),
     sessionToken: Joi.string().required().allow('', null),
     region: Joi.string().required(),
-    stage: Joi.string().required().allow('dv', 'qa', 'pp', 'ea'),
+    stage: Joi.string().required().allow('dev', 'tst', 'pre', 'prd'),
     rdsConnectionString: Joi.string().required()
   }),
   url: Joi.string().uri().required()
